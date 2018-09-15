dc4359ab4b5042de7132f42f6048d1d16db8da34
DESKTOP-DM1AOSC\wil98 <wil9876543210@hotmail.com>
2018-09-15 19:08:55 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var x = 0;
+
+function setup()
+{
+	// setup
+	createCanvas(500, 500);
+}
+
+function draw()
+{
+	// loop
+	background(0);
+	ellipse(200 * cos(x) + 250, 200 * sin(x) + 250, 100, 100);
+	x += 0.05;
+}
