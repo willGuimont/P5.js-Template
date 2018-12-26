f15ddf4ae9bf9678d128665c2c3c5ba843b64fe9
William <william.guimont-martin.1@ulaval.ca>
2018-12-25 21:54:03 -0500

    Format

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 var x = 0;
 
-function setup()
-{
+function setup() {
 	// setup
 	createCanvas(500, 500);
 }
 
-function draw()
-{
+function draw() {
 	// loop
 	background(0);
 	ellipse(200 * cos(x) + 250, 200 * sin(x) + 250, 100, 100);
